3278e3c9846bb1c35069783765b2715a92566df9
BESTIA\pixel <pxtracer@gmail.com>
2018-04-08 19:01:46 -0400

    cell sprites

--- code/u.cell.pas
+++ code/u.cell.pas
@@ -2,25 +2,26 @@ unit u.cell;
 
 interface
   uses
-    u.simcfg, u.ants, px.sdl;
+    u.simcfg, u.ants, px.sdl,  sdl2;
 
 type
   TAntInterests = (aiFood, aiCave);
-  TCellDrawMode = (dmFullCell, dmOverlay);
 
   CCell = class of TCell;
   TCell = class
     private
-      fImg :TSprite;
-      fDrawMode :TCellDrawMode;
+      fImg :PSprite;
+      fDrawOverlay :boolean;
     public
+      constructor create;
       procedure affectAnt(const ant :TAnt );virtual;abstract;
       procedure draw(x,y:integer);virtual;
+      property DrawOverlay:boolean read fDrawOverlay;
   end;
 
   TGrassCell = class(TCell)
    public
-    procedure AffectAnt(const ant :TAnt );override;
+    procedure affectAnt(const ant :TAnt );override;
   end;
 
   TInterestingCell = class(TCell)
@@ -34,20 +35,26 @@ type
     private
     public
       constructor create;
-      procedure AffectAnt(const ant :TAnt);override;
+      procedure affectAnt(const ant :TAnt);override;
   end;
 
   TFood = class(TInterestingCell)
     private
     public
       constructor create;
-      procedure AffectAnt(const ant :TAnt);override;
+      procedure affectAnt(const ant :TAnt);override;
   end;
 
   TCellFactory = class
     private
+      fOneCave :TCave;
+      fFoodImg :TSprite;
+      fCaveImg :TSprite;
     public
       procedure init;
+      procedure finalize;
+      function newFood:TFood;  //you responsible to free it later
+      function newCave:TCave;  //owned by this class
   end;
 
 var
@@ -89,16 +96,40 @@ end;
 
 { TCell }
 
+constructor TCell.create;
+begin
+  fDrawOverlay := false;
+end;
+
 procedure TCell.draw;
 begin
-  sdl.drawSprite(fImg, x,y);
+  sdl.drawSprite(fImg^ , x,y);
 end;
 
 { TCellFactory }
 
+procedure TCellFactory.finalize;
+begin
+  fOneCave.Free;
+end;
+
 procedure TCellFactory.init;
 begin
+  fFoodImg := sdl.newSprite( sdl.loadTexture('images\food01.png'));
+  fCaveImg := sdl.newSprite( sdl.loadTexture('images\cave01.png'));
+  fOneCave := TCave.create;
+  fOneCave.fImg := @fCaveImg;
+end;
 
+function TCellFactory.newCave: TCave;
+begin
+  result := fOneCave;
+end;
+
+function TCellFactory.newFood: TFood;
+begin
+  Result := TFood.create;
+  Result.fImg := @fFoodImg;
 end;
 
 initialization
